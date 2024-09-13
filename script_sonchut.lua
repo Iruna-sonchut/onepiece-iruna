3cc95ed52560073b3b7a52afd765700b15afa515
Iruna-sonchut <shopeem1500@gmail.com>
2024-09-13 14:49:25 +0700

    Update script_sonchut.lua

--- script_sonchut.lua
+++ script_sonchut.lua
@@ -1,2 +1,2 @@
-script = gg.makeRequest("https://raw.githubusercontent.com/Iruna-sonchut/onepiece-iruna/main/Basic_test.lua").content
+script = gg.makeRequest("https://raw.githubusercontent.com/Iruna-sonchut/onepiece-iruna/main/Basic_Test.lua").content
 pcall(load(script))
